d6834ea9bd059efedf56720529852ec957fcb070
patriiko <ostrunicpersona@gmail.com>
2025-05-13 16:44:08 +0200

    implement count_chars function and print to terminal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_chars
 
 
 def get_book_text(filepath):
@@ -13,8 +13,10 @@ def main():
 
     book_txt = get_book_text(book_path)
     num_of_words = count_words(book_txt)
+    num_of_chars = count_chars(book_txt)
 
     print(f"{num_of_words} words found in the document")
+    print(num_of_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def count_words(book):
     words = book.split()
 
     return len(words)
+
+
+def count_chars(book):
+    list_of_chars = list(book.lower())
+    char_dict = {}
+
+    for char in list_of_chars:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+
+    return char_dict
